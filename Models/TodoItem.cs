3162b38bfc40c10af62820c323736e765b553533
Nigel Haney <nigelhaney@microsoft.com>
2021-05-04 15:31:19 -0700

    Remove secret field

--- Models/TodoItem.cs
+++ Models/TodoItem.cs
@@ -10,8 +10,6 @@ namespace TodoApi.Models
         public string Name { get; set; }
 
         public bool IsComplete { get; set; }
-
-        public string Secret { get; set; }
     }
     public class TodoItemDTO
     {
